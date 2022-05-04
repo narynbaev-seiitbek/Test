3675076951732a933dea1481de7340fd81d991ed
seiitbeknarynbaev <narynbaev.seiitbek@gmail.com>
2022-05-04 11:02:40 +0600

    test

---
+++ src/main/java/org/peaksoft/entity/Passport.java
@@ -0,0 +1,21 @@
+package org.peaksoft.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Getter@Setter
+
+public class Passport {
+    @Id
+    @GeneratedValue()
+
+}
---
+++ src/main/java/org/peaksoft/entity/Person.java
@@ -0,0 +1,18 @@
+package org.peaksoft.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+import javax.persistence.Table;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Getter@Setter
+
+
+public class Person {
+}
